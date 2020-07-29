4de5cddb78efad0404284303749eac6b508d3ed3
DanDiCesare <daniel.dicesare22@gmail.com>
2020-07-29 18:17:23 -0400

    Adding assets
    Adding assets

---
+++ Assets/Scripts/Hand/Flick_Wrist.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+    
+ 
+ public class Flick_Wrist : MonoBehaviour {
+    Animator anim;
+    public StressReceiver m_someOtherScriptOnAnotherGameObject;
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetTrigger("Active");
+            m_someOtherScriptOnAnotherGameObject = GameObject.FindObjectOfType(typeof(StressReceiver)) as StressReceiver;
+            m_someOtherScriptOnAnotherGameObject.InduceStress(0.2f);
+            //anim.ResetTrigger("Active");
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            anim.SetTrigger("Pulling");
+            m_someOtherScriptOnAnotherGameObject = GameObject.FindObjectOfType(typeof(StressReceiver)) as StressReceiver;
+            m_someOtherScriptOnAnotherGameObject.InduceStress(0.2f);
+            //anim.ResetTrigger("Active");
+        }
+    }
+ }
---
+++ Assets/Scripts/Hand/Pull_In.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pull_In : MonoBehaviour
+{
+    public GameObject attractedTo;
+    public float strengthOfAttraction = 5.0f;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButton(1)) //Pull towards
+        {
+            Vector3 direction = attractedTo.transform.position - transform.position;
+            float strength = strengthOfAttraction - direction.magnitude + 5;
+            if (strength < 0)
+                strength = 0;
+            gameObject.GetComponent<Rigidbody>().AddForce (strength * direction);
+        }
+
+        if (Input.GetMouseButton(0)) //Push away
+        {
+            Vector3 direction = attractedTo.transform.position - transform.position;
+            float strength = strengthOfAttraction - direction.magnitude + 5;
+            if (strength < 0)
+                strength = 0;
+            gameObject.GetComponent<Rigidbody>().AddForce (strength * direction * -4 );
+        }
+        
+    }
+}
